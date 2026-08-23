b88e41e41f553fb67ab9aee831284b5f213d5bf7
hyung-wuk <2wook040809@gmail.com>
2026-08-23 22:16:05 +0900

    chore : initialize Spring Boot project

---
+++ src/main/java/com/wjsguddnr/SecLog/SecLogApplication.java
@@ -0,0 +1,13 @@
+package com.wjsguddnr.SecLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjsguddnr/SecLog/SecLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjsguddnr.SecLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
